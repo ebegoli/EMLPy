0427789b442582186d92089794d2052088f821ba
Edmon Begoli <xem@ornl.gov>
2012-04-28 10:43:27 -0400

    Added class representation for emotions

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 EmotionMLPy
-===========
+-----------
 
 Reference implementation for EmotionML markup language vocabularies in Python
 
 Structure of a EmotionML document:
 
-emotionml - root
-   emotion+
-      dimension
+emotionml - root  
+attributes: 
+namespace, version  
+   elements:
+   emotion+ 
+   attributes: 
+   category-set, dimension-set, appraisal-set, action-tendency-set
+   version
+   id? start? end? duration? time-ref-uri? time-ref-anchor-point? offset-to-start? expressed-through?
+      elements:
+      at least one of each: category, dimension, appraisal, action-tendency  
+      info (occurs only once)
--- src/emldoc.py
+++ src/emldoc.py
@@ -8,51 +8,86 @@ __author__ = 'Edmon Begoli'
 import sys
 import xml.dom.minidom
 
+trace = {'freq':2,'samples':[3,3]}
+
+class EmotionRepresentation:
+   emotions = []
+   vocabularies = []
+   info = None
+ 
+class Emotion: 
+   categories = []
+   dimensions = []
+   apprisals = []
+   action_tendencies = []
+   references = []
+   info = None
+   version = None
+   emotion_id = None
+   start = None
+   end = None
+   duration = None
+   time_ref_uri = None
+   time_ref_anchor_point = None
+   offset_to_start = None
+   expressed_through = None
+
+class Representation:
+   value = None
+   traces = []
+   name = None
+   confidence = None
+
 #TODO: make sure that dimensions passed are list of instances of documents
 def make_dimensions( doc, emotion_dimensions, trace=None, value=None, confidence=None ):
    ''' Makes an emotion sub-element dimensions. confidence 
    is optional'''
 
-   dimensions = doc.createElement('dimensions')
-   for dimension in emotion_dimensions:
-      dimensions.appendChild( dimension )
+   # dimensions = doc.createElement('dimensions')
+   # for dimension in emotion_dimensions:
+   #    dimensions.appendChild( dimension )
 
-   if trace is not None:
-      dimensions.appendChild( trace )
+   # if trace is not None:
+   #    dimensions.appendChild( trace )
       
-   if value is not None:
-      dimensions.setAttribute('value',value)
-   if confidence is not None:
-      dimension.setAttribute('confidence',confidence)
+   # if value is not None:
+   #    dimensions.setAttribute('value',value)
+   # if confidence is not None:
+   #    dimension.setAttribute('confidence',confidence)
    
-   return category   
+   # return category   
 
 def make_category( doc, value, category=None ):
    ''' Makes an emotion sub-element category. confidence 
    is optional'''
 
-   category = doc.createElement('category')
-   category.setAttribute('set', set)
-   category.setAttribute('name', name)
-   if confidence is not None:
-      category.setAttribute('confidence',confidence)
-   return category   
+   # category = doc.createElement('category')
+   # category.setAttribute('set', set)
+   # category.setAttribute('name', name)
+   # if confidence is not None:
+   #    category.setAttribute('confidence',confidence)
+   # return category   
 
-def make_emotion(category, dimension, appraisal, action_tendency, reference, info ):   
+def make_emotion(doc, children, attributes, info=None ):   
+   ''' pass children tuples, list of attributes and optional info
+       iterate through list of children tuples and attributes 
+       and append them to the doc 
+       Function enforces EmotionML constraints (has_emotion_children()) 
+   '''
+   pass
 
-
-def make_xml():
+def make_xml(emotions, vocabularies, attributes, info=None):
    ''' Makes an EmotionML compliant XML document
    '''
    doc = xml.dom.minidom.Document()
    emotionml = doc.createElement('emotionml')
    doc.appendChild(emotionml)
 
-   emotion = doc.createElement('emotion')
-   emotionml.appendChild(emotion)
+   for emotion in emotions:
+      emotionml.appendChild( make_emotion( doc, emotion ) 
 
-   category = make_category( doc, 'everydayEmotions','satisfaction')
-   emotion.appendChild(category)
+   for vocabulary in vocabularies:
+       emotionml.appendChild( make_vocabulary( doc, emotion ) 
 
    #el = doc.createElementNS('http://example.net/ns', 'el')
    #el.setAttribute("xmlns", "http://example.net/ns")
@@ -62,4 +97,9 @@ def make_xml():
 
 
 if __name__ == '__main__':
-   print make_xml().toprettyxml()
+   #print make_xml().toprettyxml()
+   em1 = EmotionRepresentation()
+   em2 = EmotionRepresentation()
+   em1.info = "test"
+   print em1.info
+
