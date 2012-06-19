6955301c57027e263b17ec8ba900c6ce14579029
Edmon Begoli <xem@ornl.gov>
2012-06-18 22:30:49 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EMLPy',
+      version='0.1',
+      description='Python library for EmotionML generation',
+      author='Edmon Begoli',
+      author_email='ebegoli@fedoraproject.org',
+      url='http://www.python.org/sigs/distutils-sig/',
+      py_modules=['emldoc'],
+     )
